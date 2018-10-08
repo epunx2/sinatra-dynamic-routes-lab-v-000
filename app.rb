59100cd8d300d18eb17ef2bfb968fbf6a26a4dc3
Eric Chrobak <eric@ericchrobak.com>
2018-10-08 23:00:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -27,7 +27,7 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     @phrase = ""
     params.each do |key, value|
-      @phrase = value + " " + @phrase
+      @phrase = @phrase + " " + value 
     end
     "#{@phrase}"
   end
